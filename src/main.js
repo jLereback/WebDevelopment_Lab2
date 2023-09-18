42739b15cf0d08ce2e37eac172256036236a4d33
jLereback <112405931+jLereback@users.noreply.github.com>
2023-09-18 23:22:49 +0200

    Import icon library

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStar1 } from "@fortawesome/free-solid-svg-icons";
+import { faStar } from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(faXmark);
+library.add(faStar1);
+library.add(faStar);
+
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
