06f4d6b50ac073562ce0812e0045be6897507eeb
jLereback <112405931+jLereback@users.noreply.github.com>
2023-09-18 23:23:09 +0200

    Update css

--- src/components/theHeader.vue
+++ src/components/theHeader.vue
@@ -4,19 +4,18 @@
 
 <template>
   <header class="logo">
-    <img alt="Vue logo" class="logo" src="../assets/todo_logo.png" height="125" />
+    <img alt="Vue logo" class="logo" src="../assets/todo_logo.png" />
   </header>
 
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto 1rem;
 }
 
+img {
+  height: 125px;
+}
 </style>
