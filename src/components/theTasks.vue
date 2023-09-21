bc8b236cbfaceb385b7210fc937c942677349a0c
jLereback <112405931+jLereback@users.noreply.github.com>
2023-09-21 09:50:24 +0200

    Add to local storage

--- src/components/theTasks.vue
+++ src/components/theTasks.vue
@@ -2,57 +2,75 @@
 import { ref } from "vue";
 
 const newTodo = ref("");
-const activeTodos = ref([]);
-const doneTodos = ref([]);
-const starredTodos = ref([]);
+const activeTodos = ref(JSON.parse(localStorage.getItem("active")) || []);
+const doneTodos = ref(JSON.parse(localStorage.getItem("done")) || []);
+const starredTodos = ref(JSON.parse(localStorage.getItem("starred")) || []);
 
 function addTodo() {
-  console.log(newTodo.value);
+  if (newTodo.value === "") return;
   activeTodos.value.push({ content: newTodo.value, active: true, done: false, starred: false });
+  updateLocalStorage();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem("active", JSON.stringify(activeTodos.value));
+  localStorage.setItem("done", JSON.stringify(doneTodos.value));
+  localStorage.setItem("starred", JSON.stringify(starredTodos.value));
 }
 
 function removeTodo(index) {
   activeTodos.value.splice(index, 1);
+  updateLocalStorage();
 }
 
 function removeDoneTodo(index) {
   doneTodos.value.splice(index, 1);
+  updateLocalStorage();
+}
+
+function removeStarredTodo(index) {
+  starredTodos.value.splice(index, 1);
+  updateLocalStorage();
 }
 
 function starTodo(todo, index) {
   todo.starred = true;
-  console.log("starred", todo);
   activeTodos.value.splice(index, 1);
   starredTodos.value.push(todo);
+  updateLocalStorage();
 }
 
 function unStarTodo(todo, index) {
   todo.starred = false;
-  console.log(index);
   starredTodos.value.splice(index, 1);
   activeTodos.value.push(todo);
-
+  updateLocalStorage();
 }
 
 function toggleTodo(todo, index) {
   todo.active = !todo.active;
   if (!todo.active) {
     todo.done = true;
-    console.log("done", todo);
-    activeTodos.value.splice(index, 1);
+    if (todo.starred === true) {
+      todo.starred = false;
+      starredTodos.value.splice(index, 1);
+    } else {
+      activeTodos.value.splice(index, 1);
+    }
     doneTodos.value.push(todo);
+    updateLocalStorage()
   } else {
     todo.done = false;
-    console.log("active", todo);
     doneTodos.value.splice(index, 1);
     activeTodos.value.push(todo);
+    updateLocalStorage();
   }
 }
 </script>
 
 <template>
   <div class="container">
-    <form @submit.prevent="addTodo()">
+    <form @submit="addTodo()">
       <input class="form" type="text" v-model="newTodo" placeholder="Enter text">
       <button class="form">Add Todo</button>
     </form>
@@ -63,8 +81,8 @@ function toggleTodo(todo, index) {
           <h3 :class="{ starred: todo.starred }" @click="toggleTodo(todo, index)">{{ todo.content }}</h3>
         </div>
         <div>
-        <font-awesome-icon icon="fa-solid fa-star" @click="unStarTodo(todo, index)" class="icon" />
-          <font-awesome-icon icon="fa-solid fa-xmark" @click="removeTodo(index)" class="icon" />
+          <font-awesome-icon icon="fa-solid fa-star" @click="unStarTodo(todo, index)" class="icon" />
+          <font-awesome-icon icon="fa-solid fa-xmark" @click="removeStarredTodo(index)" class="icon" />
         </div>
       </li>
     </ul>
